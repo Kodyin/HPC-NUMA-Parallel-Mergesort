08a159f5315ff9337010dea19fc13b4c2540c67a
Kodyin <464159206@qq.com>
2018-02-01 16:25:44 -0800

    tr

--- parallel-mergesort.cc
+++ parallel-mergesort.cc
@@ -5,176 +5,147 @@
  */
 
 #include <assert.h>
-#include <omp.h>
 #include <stdio.h>
 #include <stdlib.h>
-#include <iostream>
-#include <string.h>
-using namespace std;
-
+#include <cstring>
+#include <omp.h>
 #include "sort.hh"
-void
-parallelSort (int N, keytype* A);
-void
-mergeSort (keytype* A, int l, int r, keytype* B, int s, int lvl);
-// void
-// mergeSort (keytype* A, int l, int r, int lvl);
-void
-merge (keytype* A, int l1, int r1, int l2, int r2, keytype* B, int l3, int lvl);
-int
-binaryS (int tar, keytype* A, int l, int r);
-void 
-MSort(keytype* A,keytype* T,int l,int r);
-void
-mergeSequential (keytype* A, int l1, int r1, int l2, int r2, keytype* B, int l3);
 
-
-static int compare (const void* a, const void* b)
-{
-  keytype ka = *(const keytype *)a;
-  keytype kb = *(const keytype *)b;
-  if (ka < kb)
-    return -1;
-  else if (ka == kb)
-    return 0;
-  else
-    return 1;
-}
-void sequentialQSort (int N, keytype* A, keytype* B, int s)
-{
-  qsort (A, N, sizeof (keytype), compare);
-  memcpy (B+s, A, (N) * sizeof (keytype));
+/**
+ * Searches for an index Q in 2nd array where
+ * all elements with index < Q are less than mid element
+ * of first array
+ */
+int splitPoint(keytype* &arr, int first, int last, int mid1) {
+    int ans = 0;
+    while (last > first) {
+        ans = (first + last) / 2;
+        if (arr[ans] >= arr[mid1]) {
+            last = ans;
+        } else {
+            first = ans + 1;
+        }
+    }
+    return last;
 }
 
+/**
+ * parallel merge which returns the result in auxiliary array
+ * auxiliary array (aux) is shared between different recursions,
+ * so merge functions uses the space corresponding to the main array
+ * indicated by auxOffset as starting index
+ * and the length determined by the sum sizes of both sub arrays.
+ * s1, e1 are start and end of sub array1. s2, e2 are start and end of sub array2.
+ * sz1 and sz2 are sizes of sub array1 and sub array2 respectively.
+ */
+void pMerge(keytype* &arr, keytype* &aux, int auxOffset, int s1, int e1, int s2,
+        int e2, int depth) {
+
+    int sz1 = e1 - s1 + 1, sz2 = e2 - s2 + 1;
+
+    /*use recursion if there are more than 2 elements in both arrays
+     * and number of threads available is greater than 1
+     * else fall back to sequential(non-recursive merge)
+     */
+    if (sz1 > 2 && sz2 > 2 && depth > 1) {
+
+        int m1 = (s1 + e1) / 2;
+        int m2 = splitPoint(arr, s2, e2 + 1, m1);
+        int qOffset = 0;
+
+        if (m2 == e2 + 1) {
+            /*all elements of array 2 are less than mid element of array 1*/
+            qOffset = auxOffset + (m1 - s1) + (e2 - s2 + 1);
+            memcpy(aux + qOffset, arr + m1, (e1 - m1 + 1) * sizeof(keytype));
+            pMerge(arr, aux, auxOffset, s1, m1 - 1, s2, e2, depth);
+            return;
+        } else if (m2 == s2) {
+            /*all elements of array2 are greater than mid element of array 1*/
+            memcpy(aux + auxOffset, arr + s1, (m1 - s1 + 1) * sizeof(keytype));
+            pMerge(arr, aux, auxOffset + (m1 - s1 + 1), m1 + 1, e1, s2, e2,
+                    depth);
+            return;
+        } else {
+            qOffset = auxOffset + (m1 - s1) + (m2 - s2);
+            aux[qOffset] = arr[m1];
+#pragma omp task shared(arr, aux, depth)
+            {
+                pMerge(arr, aux, auxOffset, s1, m1 - 1, s2, m2 - 1, depth / 2);
+            }
+            pMerge(arr, aux, qOffset + 1, m1 + 1, e1, m2, e2, depth / 2);
+#pragma omp taskwait
+        }
+    } else {
+        /*sequential merge*/
+        int itr1 = s1, itr2 = s2, limit = auxOffset + sz1 + sz2;
+        for (int i = auxOffset; i < limit; i++) {
+            if (itr1 > e1) {
+                memcpy(aux + i, arr + itr2, (limit - i) * sizeof(keytype));
+                break;
+            } else if (itr2 > e2) {
+                memcpy(aux + i, arr + itr1, (limit - i) * sizeof(keytype));
+                break;
+            } else if (arr[itr1] < arr[itr2]) {
+                aux[i] = arr[itr1];
+                itr1++;
+            } else {
+                aux[i] = arr[itr2];
+                itr2++;
+            }
+        }
+    }
+    return;
+}
+
+/*Parallel Merge sort*/
+void mergeSort(keytype* &arr, keytype* &aux, int start, int end, int depth) {
+
+    if (start >= end)
+        return;
+
+    int mid = start + ((end - start) / 2);
+    /*spawn new threads for recursion
+     * if available threads are greater than 1
+     */
+    if (depth > 1) {
+#pragma omp task shared(arr, aux)
+        {
+            mergeSort(arr, aux, start, mid, depth / 2);
+        }
+        mergeSort(arr, aux, mid + 1, end, depth / 2);
+#pragma omp taskwait
+    } else {
+        mergeSort(arr, aux, start, mid, depth);
+        mergeSort(arr, aux, mid + 1, end, depth);
+    }
+
+    pMerge(arr, aux, start, start, mid, mid + 1, end, depth);
+    memcpy(arr + start, aux + start, (end - start + 1) * sizeof(keytype));
+
+    return;
+}
 
 void
 parallelSort (int N, keytype* A)
 {
-    //omp_set_nested(1);
-    //omp_set_num_threads (4);
+    /*depth is used to avoid spawning more threads than available in system*/
     int depth = 0;
-    //for(int i = 0; i<N; i++) printf("%d\n",A[i]);
-    //keytype* ans = newKeys(N);
-    #pragma omp parallel
+
+    /*Auxiliary array for extra space needed during merge*/
+    keytype *aux = (keytype *) malloc(N * sizeof(keytype));
+
+#pragma omp parallel
     {
-         #pragma omp single nowait
+#pragma omp single nowait
         {
+            /*get the number of threads in the system*/
             depth = omp_get_num_threads();
-            mergeSort (A, 0, N-1, A, 0, depth);
+            mergeSort(A, aux, 0, N - 1, depth);
         }
-              
     }
-    //for(int i = 0; i<N; i++) printf("%d\n",A[i]);
-  /* Lucky you, you get to start from scratch */
+
+    free(aux);
+
 }
-
-void
-mergeSort (keytype* A, int l, int r, keytype* B, int s, int lvl){
-        int n = r - l + 1;
-        //printf("%d\n",n);
-        if(n<1) 
-            return;
-        else if(n==1){
-            B[s] = A[l];
-        }
-        else {
-            int mid = (l+r)>>1, len = mid - l + 1;
-            if(lvl>1)
-            {
-                keytype* T = newKeys(n);
-                #pragma omp task
-                mergeSort(A, l, mid, T, 0, lvl/2);
-                mergeSort(A, mid+1, r, T, len, lvl/2);
-                #pragma omp taskwait
-                merge(T, 0, len-1, len, n-1, B, s, lvl);
-                free(T);
-            }
-            else {
-                keytype* T = newKeys(r+1);
-                MSort(A,T,l,r);
-                memcpy(B+s, A+l, (r-l+1) * sizeof(keytype));;
-                free(T);
-                //sequentialQSort (mid-l+1, A+l, T, 0);
-                //sequentialQSort (r-mid, A+mid+1, T, len);
-                //mergeSort(A, l, mid, T, 0, lvl);
-                //mergeSort(A, mid+1, r, T, len, lvl);
-            }
-        }
-    
-}
-int
-binaryS (int tar, keytype* A, int l, int r){
-    int low = l, high = max(l, r+1);
-    while(low<high){
-        int mid = (low+high)>>1;
-        if(tar<=A[mid]) high = mid;
-        else low = mid + 1;
-    }
-    return high;
-}
-
-void
-merge (keytype* A, int l1, int r1, int l2, int r2, keytype* B, int l3, int lvl){
-        //if(lvl<4) printf("Thread %d is merging %d ~ %d with %d ~ %d \n", omp_get_thread_num(), l1, r1, l2, r2); 
-        int n1 = r1 - l1 + 1, n2 = r2 - l2 + 1;
-        if(lvl>1){
-            if(n1<n2){
-                swap(l1,l2);
-                swap(r1,r2);
-                swap(n1,n2);
-            }
-            if(n1<=0) return;
-            int mid1 = (l1+r1)>>1;
-            int mid2 = binaryS(A[mid1], A, l2, r2);
-            int mid3 = l3 + (mid1 - l1) + (mid2 - l2);
-            B[mid3] =  A[mid1];
-
-            #pragma omp task 
-            merge(A, l1, mid1-1, l2, mid2-1, B, l3, lvl/2);
-            merge(A, mid1+1, r1, mid2, r2, B, mid3+1, lvl/2);
-            #pragma omp taskwait   
-        }
-        else {
-            mergeSequential(A, l1, r1, l2, r2, B, l3);
-        }                               
-}
-
-void MSort(keytype* A,keytype* T,int l,int r)
-{
-    int mid;
-    if(l<r)
-    {
-        mid=(l+r)/2;
-        /* Sequential */
-        MSort(A,T,l,mid);
-        MSort(A,T,mid+1,r);
-        mergeSequential(A,l,mid,mid+1,r, T, l);
-        memcpy(A+l, T+l, (r-l+1) * sizeof(keytype));
-    }
-}
-
-void
-mergeSequential (keytype* A, int l1, int r1, int l2, int r2, keytype* B, int l3){
-    int a = l1, b = l2;
-    for(int i = 0; i<r1-l1+1+r2-l2+1; i++){
-        if(a>r1) {
-            memcpy(B + l3 + i, A + b, (r2 - b + 1) * sizeof(keytype));
-            break;
-        }
-        else if(b>r2) {
-            memcpy(B + l3 + i, A + a, (r1 - a + 1) * sizeof(keytype));
-            break;
-        }
-        else if(A[a] < A[b]) B[l3+i] = A[a++];
-        else B[l3+i] = A[b++];
-    }
-}
-
-
-//if(lvl<4) printf("Thread %d is sorting %d through %d\n", omp_get_thread_num(), l, r);  
-            // printf("%d\n",omp_get_max_threads());
-            // printf("Num Threads:%d ",omp_get_num_threads());
-            // printf("id:%d\n",omp_get_thread_num());
-                //keytype* T = newKeys(n);
 
 /* eof */
